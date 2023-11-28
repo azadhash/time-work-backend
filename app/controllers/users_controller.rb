5d34eabbcc43151155df37f47fce7bf6eee88555
Azad Singh <azad.singh@kreeti.com>
2023-11-28 15:46:55 +0530

    created user create and destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    @user.save
+    render json: @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    render json: @user
+  end
 end
